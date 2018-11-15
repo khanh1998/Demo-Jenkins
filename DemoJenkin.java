cfc74ce11dfa63be28fef8e9e8911e3646e7a54c
Khanh <quockhanh.abc@gmail.com>
2018-11-15 16:22:56 +0700

    first commit

---
+++ DemoJenkin.java
@@ -0,0 +1,11 @@
+public class DemoJenkin {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("This is Demo Jenkin");
+        System.out.println("Demo Jenkin");
+    }
+    
+}
