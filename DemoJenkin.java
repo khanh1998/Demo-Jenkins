36f1c5a5511f0f0f794b2928c0f4f045bbfedb68
Huangshan <sonpham3798@gmail.com>
2018-11-15 15:55:44 +0000

    Recover

--- DemoJenkin.java
+++ DemoJenkin.java
@@ -9,10 +9,6 @@ public class DemoJenkin {
         System.out.println("This is Demo Jenkin");
         System.out.println("Demo Jenkin");
         System.out.println("This demo is make by a handsome boy");
-
-        List<String> newList = new ArrayList<>();
-        newList.add("This text make by a GOOD BOY :)");
-
     }
     
 }
